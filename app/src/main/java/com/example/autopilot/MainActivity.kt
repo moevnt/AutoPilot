702cc59bb1f816d5ea8f1d6d57688f76bc1b3c71
moevnt <evan.monteith@gmail.com>
2020-12-07 22:04:25 -0500

    report Button

--- app/src/main/java/com/example/autopilot/MainActivity.kt
+++ app/src/main/java/com/example/autopilot/MainActivity.kt
@@ -4,31 +4,19 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
 import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
 
 class MainActivity : AppCompatActivity() {
-	
-	private lateinit var appBarConfiguration: AppBarConfiguration
 
 	private lateinit var signOut : Button
 	private lateinit var addButton : Button
 	private lateinit var withdrawalButton: Button
 	private lateinit var graphView : GraphView
+	private lateinit var reportButton: Button
 
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,17 +26,11 @@ class MainActivity : AppCompatActivity() {
 		graphView  = findViewById(R.id.graph)
 		addButton = findViewById(R.id.Add_Button)
 		withdrawalButton = findViewById(R.id.Withdrawal_Button)
+		reportButton = findViewById(R.id.Report_Button)
 		signOut = findViewById(R.id.Sign_Out_Button)
 		
 		graphView
 		graphView.visibility
-		
-		
-		
-		signOut.setOnClickListener { view : View ->
-			val intent = LoginActivity.newIntent(this@MainActivity)
-			startActivity(intent)
-		}
 
 		addButton.setOnClickListener { view : View ->
 			val intent = AddFundsActivity.newIntent(this@MainActivity)
@@ -57,6 +39,18 @@ class MainActivity : AppCompatActivity() {
 
 		withdrawalButton.setOnClickListener { view : View ->
 			val intent = WithdrawalActivity.newIntent(this@MainActivity)
+			startActivity(intent)
+		}
+		
+		reportButton.setOnClickListener {
+			val toast: Toast =
+				Toast.makeText(this, "Report in inbox", Toast.LENGTH_SHORT)
+			toast.setGravity(Gravity.TOP, 0, 0)
+			toast.show()
+		}
+		
+		signOut.setOnClickListener { view : View ->
+			val intent = LoginActivity.newIntent(this@MainActivity)
 			startActivity(intent)
 		}
 	}
@@ -68,16 +62,5 @@ class MainActivity : AppCompatActivity() {
 
 			return intent
 		}
-	}
-	
-	override fun onCreateOptionsMenu(menu: Menu): Boolean {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		menuInflater.inflate(R.menu.options, menu)
-		return true
-	}
-	
-	override fun onSupportNavigateUp(): Boolean {
-		val navController = findNavController(R.id.nav_host_fragment)
-		return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 	}
 }
